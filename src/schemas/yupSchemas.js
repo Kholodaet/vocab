bc80cd1036c5b9c46344250bb95a45ca070daf4c
Kholodaet <kholodaet@icloud.com>
2025-01-22 16:07:59 +0200

    Update yupSchemas.js

--- src/schemas/yupSchemas.js
+++ src/schemas/yupSchemas.js
@@ -4,21 +4,25 @@ export const registerSchema = Yup.object().shape({
   name: Yup.string().required("Name is required"),
   email: Yup.string()
     .email("Invalid email address")
+    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)
     .required("Email is required"),
   password: Yup.string()
-    .min(6, "Password must be at least 6 characters long")
-    .matches(/[a-zA-Z]/, "Password must include at least one letter")
-    .matches(/\d/, "Password must include at least one number")
+    .matches(
+      /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/,
+      "min 6, including one letter and one number"
+    )
     .required("Password is required"),
 });
 
 export const loginSchema = Yup.object().shape({
   email: Yup.string()
     .email("Invalid email address")
+    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)
     .required("Email is required"),
   password: Yup.string()
-    .min(6, "Password must be at least 6 characters long")
-    .matches(/[a-zA-Z]/, "Password must include at least one letter")
-    .matches(/\d/, "Password must include at least one number")
+    .matches(
+      /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/,
+      "min 6, including one letter and one number"
+    )
     .required("Password is required"),
 });
